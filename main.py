34f4d43ba577f56f28eaa42d84c31025fb22f556
Ryan Pham <rpham817@gmail.com>
2026-09-07 07:33:16 -0700

    added distance calculations

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import numpy as np
 import cv2 as cv
 from ultralytics import YOLO as yolo
 
-#model = yolo("models/drone-yolo26m.pt") #load a pretrained yolo model
-model = yolo("models/yolo26n.pt") #use to test with cars
+model = yolo("models/drone-yolo26m.pt") #load a pretrained yolo model
+#model = yolo("models/yolo26n.pt") #use to test with cars
 
 #create a VideoCapture object (can access files OR camera (use 0 for camera, 1 for external camera, etc.))
-cap = cv.VideoCapture("videos/testroad.mp4") 
+cap = cv.VideoCapture("videos/test2.mp4") 
 
 #make sure the video file is opened successfully
 if not cap.isOpened():
     print("Error: Could not open video.")
     exit()
+
+fps = cap.get(cv.CAP_PROP_FPS) #get the frames per second of the video
 
 track_history = {} #dictionary to store the track history of each object
 
@@ -44,6 +46,18 @@ while cap.isOpened():
                 track_history[track_id] = [] #initialize the track history for this object
 
             track_history[track_id].append((int(x), int(y)))
+
+            #calculate the distance moved by the object since the last frame
+            if len(track_history[track_id]) > 2: 
+                previous_x, previous_y = track_history[track_id][-2]
+                current_x, current_y = track_history[track_id][-1]
+
+                dx = current_x - previous_x
+                dy = current_y - previous_y
+
+                distance = np.sqrt(dx**2 + dy**2)
+
+                #print(f"Track ID: {track_id}, dx: {dx}, dy: {dy}, Distance moved: {distance:.2f} pixels")
 
             if len(track_history[track_id]) > 50:
                 track_history[track_id].pop(0)
