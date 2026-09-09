14a558f07609ddda2179deba1fc5d51ad78a7a49
Ryan Pham <rpham817@gmail.com>
2026-09-09 02:05:41 -0700

    updated tracking to remove stale objects

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import numpy as np
 import cv2 as cv
 from ultralytics import YOLO as yolo
 
-model = yolo("models/drone-yolo26m.pt") #load a pretrained yolo model
-#model = yolo("models/yolo26n.pt") #use to test with cars
+#model = yolo("models/drone-yolo26m.pt") #load a pretrained yolo model
+model = yolo("models/yolo26n.pt") #use to test with cars
 
 #create a VideoCapture object (can access files OR camera (use 0 for camera, 1 for external camera, etc.))
-cap = cv.VideoCapture("videos/test.mp4") 
+cap = cv.VideoCapture("videos/testroad.mp4") 
 
-#make sure the video file is opened successfully
 if not cap.isOpened():
     print("Error: Could not open video.")
     exit()
 
 fps = cap.get(cv.CAP_PROP_FPS) #get the frames per second of the video
 
-track_history = {} #dictionary to store the track history of each object
-telemetry = {} #dictionary to store the telemetry data of each object
+track_history = {} #store the track history of each object
+telemetry = {} #store the telemetry data of each object
+last_seen = {} #store the last seen frame of each object (key = track_id, value = last seen frame)
 
-#run while loop to read frames from the video
+current_frame = 0
+max_missing_frames = 30 #max frames an object can be missing before it is considered lost
+
+#run while loop to read frames from the video (frame by frame)
 while cap.isOpened():
     success, frame = cap.read() #read a frame from the video
     
@@ -26,7 +29,9 @@ while cap.isOpened():
         print("Could not read frame (stream ended). Exiting...")
         break
 
-    result = model.track( #add tracking to the model. provide the frame to track
+    current_frame += 1
+
+    result = model.track( #run the model on the frame and get the results (detections). track too
         frame,
         persist = True,
         tracker = "bytetrack.yaml",
@@ -34,15 +39,17 @@ while cap.isOpened():
         conf = 0.20,
         verbose = False,
         device = 0,
-        quantize = 16)[0] #run the model on the frame and get the results (detections)
+        quantize = 16)[0] 
 
+    #work through every tracked object in the curr frame
     if result.boxes.id is not None:
-        track_ids = result.boxes.id.int().cpu().tolist() #get the track IDs of the detected objects
-        boxes = result.boxes.xywh.cpu().tolist() #get the bounding boxes of the detected objects
+        track_ids = result.boxes.id.int().cpu().tolist() #get the track IDs/bounding boxes of the detected objects
+        boxes = result.boxes.xywh.cpu().tolist()
 
         #update the track history and telemetry data for each detected object
         for box, track_id in zip(boxes, track_ids):
             x, y, width, height = box #unpack the bounding box coordinates (x, y, width, height)
+            last_seen[track_id] = current_frame #update the last seen frame for each object in the curr frame
 
             if track_id not in track_history:
                 track_history[track_id] = [] #initialize the track history for this object
@@ -78,6 +85,19 @@ while cap.isOpened():
             if len(track_history[track_id]) > 50:
                 track_history[track_id].pop(0)
 
+    #check for stale tracks to remove tracking
+    stale_tracks = []
+
+    for track_id, last_frame in last_seen.items():
+        if current_frame - last_frame > max_missing_frames:
+            stale_tracks.append(track_id)
+
+    #pop the stale tracks from the dictionaries
+    for track_id in stale_tracks:
+        track_history.pop(track_id, None)
+        telemetry.pop(track_id, None)
+        last_seen.pop(track_id, None)
+
     annotated_frame = result.plot()
 
     for track_id, points in track_history.items(): #.items() lets you access key, value
@@ -97,5 +117,5 @@ while cap.isOpened():
     if cv.waitKey(1) & 0xFF == ord('q'): #play the video (1ms frame switch) until 'q' is pressed
         break       
 
-cap.release() #release the video capture object
-cv.destroyAllWindows() #close all OpenCV windows
+cap.release()
+cv.destroyAllWindows()
